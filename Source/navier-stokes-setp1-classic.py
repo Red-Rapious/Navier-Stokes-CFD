bb53badf5a7097c5b8f56931681ad51a46952875
Red Rapious <red.rapious@gmail.com>
2021-11-16 17:41:30 +0100

    Utilisation du module d'animation de matplotlib

--- Source/navier-stokes-setp1-classic.py
+++ Source/navier-stokes-setp1-classic.py
@@ -5,15 +5,17 @@ Référence : lorenabarba.com/blog/cfd-python-12-steps-to-navier-stokes/
 
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation 
 
-"""Etape 1 : Convection linéaire en 1 dimension - version animée avec matplotlib"""
+"""Etape 1 : Convection linéaire en 1 dimension"""
 
-BLIT = True
+ANIMATED = True
+CLEAR = False
 
-fig, axes = plt.subplots(1,1, figsize=(8,8))
-axes.set_title("Animation de déplacement d'une vague en 1 dimension à convection linéaire")
-
+if ANIMATED:
+    fig, axes = plt.subplots(1,1, figsize=(8,8))
+    axes.set_title("Animation de déplacement d'une vague en 1 dimension à convection linéaire")
+else:
+    plt.title("Chronographie du déplacement d'une vague en 1 dimension à convection linéaire")
 
 # Constantes
 nx = 41 # nombre de points
@@ -29,19 +31,18 @@ u[int(.5/dx):int(1/dx+1)]=2 # niveau haut (entre .5 et 1)
 
 # Calcul de u en fonction du temps : n et n+1 sont deux instants consécutifs
 u_n = np.ones(nx)
-line, = axes.plot(np.linspace(0,2,nx), u)
 
-# Animation 
-def animate(n):
-    global u_n
-    global line
-
+for n in range(nt): # nombre d'étapes temporelles
     u_n = u.copy()
     for i in range(1, nx): # on commence à 1 car u_n dépend de u_n-1
         u[i] = u_n[i] - c*dt/dx*(u_n[i] - u_n[i-1])
-    line.set_data(np.linspace(0,2,nx), u)
-    return line,
-
-anim = FuncAnimation(fig, animate, frames = nt, interval = dt, blit = BLIT)
+    
+    if ANIMATED:
+        if CLEAR:
+            axes.clear()
+        axes.plot(np.linspace(0,2,nx), u)
+        plt.pause(dt)
+    else:
+        plt.plot(np.linspace(0,2,nx), u)
 
 plt.show()
